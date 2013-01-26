6f2528ef203c4fae6e7237ef5140499545712c0e
Bert Hartmann <BertHartm@gmail.com>
2013-01-26 18:25:13 -0500

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NJEduThing::Application.config.secret_token = '6bb6d413abbaf9dde8446e3d8c31fa13f263c2036b3b02a9448771d5bc7bfec9c65aa1ae69c3782e3a48aee41d15b33a6c7149a9630f36b6a02c32f9fae9fe5a'
