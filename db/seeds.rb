bb91946788070075fa1349d2608a282ad078918d
Bert Hartmann <BertHartm@gmail.com>
2013-01-26 21:35:24 -0500

    add structure and partial see setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+File.open("schlsums.txt") do |f|
+  f.readline() # skip the first line (headers)
+  f.each_line do |tsv|
+    tsv.chomp!
+    arr tsv.split('\t')
+    schl = arr[2].strip
+    if not schl.nil?
+      x = School.new(:county_id => arr[0].strip,
+                     :district_id => arr[1].strip,
+                     :school_id => arr[2].strip,
+                     :county => arr[3].strip,
+                     :district => arr[4].strip,
+                     :name => arr[5].strip,
+                     :dfg => arr[6].strip,
+                     :numstudents => arr[7],
+                     :numpassing => arr[8])      
+    else
+      x = District.new(
+                       #TODO: this)
+      
+    end
+  end
+end
+
+File.open("dfgsums.txt") do |f|
+  f.readline() # skip the first line (headers)
+  f.each_line do |tsv|
+    tsv.chomp!
+    arr tsv.split('\t')
+    schl = arr[2].strip
+    x = Dfg.new(:label => arr[0].strip,
+                :passrate => arr[3].strip)
+  end
+end
+
+File.open("drpout.txt") do |f|
+  f.readline() # skip the first line (headers)
+  f.each_line do |tsv|
+    tsv.chomp!
+    arr tsv.split('\t')
+    schl = arr[2].strip
+    x = Dfg.new(:CO_CODE => arr[0].strip,
+                :DIST_CODE => arr[1].strip,
+                :SCH_CODE => arr[2].strip,
+                :TOTDRP => arr[3].strip)
+  end
+end
